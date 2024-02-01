0853cf9558cb4aa39827e6c060fb8bd311566f65
dpw360 <dpw360@gmail.com>
2024-01-31 20:50:37 -0800

    For loop implemented, getting error on Out-File

--- IT120/PowerShell/objectLab.ps1
+++ IT120/PowerShell/objectLab.ps1
@@ -1,3 +1,11 @@
+Write-Host "Please wait, currently finding .dll's ..."
 $list = Get-ChildItem -Path 'C:\Windows' -Filter '*.dll' -Recurse
+
 $numFiles = $list.Count
-Write-Host $numFiles
+Write-Host "You have $numFiles's dll's in you C:\Windows directory.`nPlease wait while your .csv is generated"
+
+New-Item -Path 'C:\Users\Drew\Documents\dll.csv' -ItemType File
+
+for ($i = $numFiles - 1; $i -gt $numFiles - 100; $i--) {
+    Out-File -FilePath 'C:\Users\Drew\Documents\dll.csv' $list[$i].Name
+}
