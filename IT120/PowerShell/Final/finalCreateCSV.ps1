f25e99a39f61a17705669a5a254b8f250912f44d
dpw360 <dpw360@gmail.com>
2024-02-05 13:00:11 -0800

    Renamed the file, and finished it with some formatting. Just needs comments.

--- IT120/PowerShell/Final/finalCreateCSV.ps1
+++ IT120/PowerShell/Final/finalCreateCSV.ps1
@@ -1,11 +1,12 @@
 # Goal Output
 # "Password","First","Last","Userid","FullName"
 New-Item -Path '.\Final.csv' -ItemType File
+Add-Content -Path '.\Final.csv' -Value "Password First Last Userid FullName"
 
 foreach ($line in Get-Content -Path .\Final.txt) {
     $items = $line -split ' '
 
-    $password = $items[0]
+    $password = $items[0] + "-000"
     $first = $items[1]
     $last = $items[2]
     $userID = $items[3]
