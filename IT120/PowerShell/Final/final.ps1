e2e425f1227b0bfe0d843164ae18c87b142f19cf
dpw360 <dpw360@gmail.com>
2024-02-05 12:56:07 -0800

    Got the loop working, it now successfully outputs to the .csv in the correct format.

--- IT120/PowerShell/Final/final.ps1
+++ IT120/PowerShell/Final/final.ps1
@@ -1,26 +1,15 @@
 # Goal Output
 # "Password","First","Last","Userid","FullName"
-# "734-44-2041-000","James","Smith","jsmith","James Smith"
-# "655-84-8254-000","John","Johnson","jjohnson","John Johnson"
-# "138-49-0705-000","Robert","Williams","rwilliams","Robert Williams"
-# "329-17-8632-000","Michael","Jones","mjones","Michael Jones"
+New-Item -Path '.\Final.csv' -ItemType File
 
-# New-Item -Path '.\Final.csv' -ItemType File
+foreach ($line in Get-Content -Path .\Final.txt) {
+    $items = $line -split ' '
 
-$i = 0
-$currentFirst = $currentLast = ""
-
-foreach ($item in Get-Content -Path .\Final.txt) {
-    $item | Select-String -Pattern "^*" -AllMatches
+    $password = $items[0]
+    $first = $items[1]
+    $last = $items[2]
+    $userID = $items[3]
+    $fullName = "$first $last"
+    
+    Add-Content -Path '.\Final.csv' -Value "$password, $first, $last, $userID, $fullName"
 }
-
-## Gives the following wrong output:
-# 734-44-2041, James, Smith, jsmith
-# 655-84-8254, James Smith
-# John, Johnson, jjohnson
-# 138-49-0705, Robert, Johnson jjohnson
-# 138-49-0705
-# Williams, rwilliams
-# 329-17-8632, Michael, Jones, rwilliams
-# 329-17-8632 Michael
-# mjones
