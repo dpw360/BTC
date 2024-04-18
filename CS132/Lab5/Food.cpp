2abb67f79befda010e4b1ca68c923bb4402a05c9
dpw360 <dpw360@gmail.com>
2024-04-18 11:05:19 -0700

    Cleaned up some comments, all tested!

--- CS132/Lab5/Food.cpp
+++ CS132/Lab5/Food.cpp
@@ -33,6 +33,8 @@ bool Food::hasFlavor(string flavorSearch) {
 }
 
 // Overloads the + operator to create a new Food object from two Food objects.
+// I do this by creating the new information for name, calories, and flavors,
+// then creating and returning a new Food object with that information.
 Food Food::operator+(Food f) {
     string newName = this->name + " & " + f.name;
     int newCal = this->calories + f.calories;
