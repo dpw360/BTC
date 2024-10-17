88493d3857f0dbe52dde332f0ca0d5426623adaa
dpw360 <dpw360@gmail.com>
2024-10-17 14:09:58 -0700

    PlayATune is working! Created musicNotes.cpp definitions in.

--- IT250/PlayATune/src/PlayATune.cpp
+++ IT250/PlayATune/src/PlayATune.cpp
@@ -1,45 +1,62 @@
+#include <clickButton.h>
 #include "Particle.h"
+#include "musicNotes.cpp"
 #define BUZZER D1
 #define LED D2
-#define BUTTON D3
-bool buttonState = false;
+#define BUTTON_PIN D3
+
+ClickButton BUTTON(BUTTON_PIN, LOW, CLICKBTN_PULLUP);
 
 
 void setup() {
   pinMode(BUZZER, OUTPUT);
   pinMode(LED, OUTPUT);
-  pinMode(BUTTON, INPUT);
+  pinMode(BUTTON_PIN, INPUT);
 }
 
-// INPUT: N/A
-// OUTPUT: A boolean describing whether or not the button was pressed.
-bool wasButtonPressed() {
-  // Read if the button was pressed, store in var
-  bool pressed = digitalRead(BUTTON);
+void playSong(int songID) {
+  digitalWrite(LED, HIGH);
 
-  // Polls with an interrupt
-  if (!pressed) {
-    buttonState = 1 - buttonState;
-    delay(200);
-  }
+  if (songID % 2 == 1) {
+    int melody[] = {N_B7,N_D6,N_GS6,N_A7,N_D6,N_E6,N_GS6,N_A6,N_B7,N_C7,N_B7,N_E6};  // notes in the melody
+    int noteDurations[] = {6,6,6,6,6,6,6,6,6,6,6,6};               // note durations
 
-  // Return if the button was pressed
-  if (buttonState) {
-    return true;
+    for (int thisNote = 0; thisNote < 12; thisNote++) {      // ergodic all notes
+      int noteDuration = 1000/noteDurations[thisNote];      // calculate the note duration
+      tone(BUZZER, melody[thisNote], noteDuration);         // let speaker sonds
+      int pauseBetweenNotes = noteDuration * 1.30;          // set a minimum time between notes
+      delay(pauseBetweenNotes);                             // delay for the while
+      noTone(BUZZER);                                       // stop the tone playing:
+    }
   } else {
-    return false;
+    int melody[] = {1900,2000,2000,2443,2500,0,2000,1900};  // notes in the melody
+    int noteDurations[] = {4,8,8,4,4,4,4,4 };               // note durations
+
+    for (int thisNote = 0; thisNote < 8; thisNote++) {      // ergodic all notes
+      int noteDuration = 1000/noteDurations[thisNote];      // calculate the note duration
+      tone(BUZZER, melody[thisNote], noteDuration);         // let speaker sonds
+      int pauseBetweenNotes = noteDuration * 1.30;          // set a minimum time between notes
+      delay(pauseBetweenNotes);                             // delay for the while
+      noTone(BUZZER);  
+    }
   }
+
+  digitalWrite(LED, LOW);  
 }
 
-int melody[] = {1908,2551,2551,2273,2551,0,2024,1908};  // notes in the melody
-int noteDurations[] = {4,8,8,4,4,4,4,4 };               // note durations
+bool clicked;
+int songID = 0;
 
 void loop() {
-  for (int thisNote = 0; thisNote < 8; thisNote++) {  // ergodic all notes
-    int noteDuration = 1000/noteDurations[thisNote];  // calculate the note duration
-    tone(BUZZER, melody[thisNote], noteDuration);  // let speaker sonds
-    int pauseBetweenNotes = noteDuration * 1.30;      // set a minimum time between notes
-    delay(pauseBetweenNotes);                         // delay for the while
-    noTone(BUZZER);                                // stop the tone playing:
+  clicked = false;
+  BUTTON.Update();
+  
+  if (BUTTON.clicks != 0) {
+    clicked = true;
+    songID++;
+  }
+
+  if (clicked) {
+    playSong(songID);
   }
 }
